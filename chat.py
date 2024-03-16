1f362e8c1f5f594586d5a8014fd49c85499da9ba
Nicola <nmanzini@users.noreply.github.com>
2024-03-15 21:48:40 -0500

    chat with antropic

--- chat.py
+++ chat.py
@@ -3,7 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SimpleChatEngine.from_defaults(llm=)
 chat_engine = SimpleChatEngine.from_defaults()
 
 response = chat_engine.chat("how old was napoleon when he died?")
@@ -12,4 +11,22 @@ print(response)
 
 response = chat_engine.chat("can you translate what you jsut said to italian?")
 
+
 print(response)
+
+from llama_index.llms.anthropic import Anthropic
+from llama_index.core.base.llms.types import ChatMessage
+from llama_index.core.memory import ChatMemoryBuffer
+
+llm = Anthropic()
+
+prefix_messages = [ChatMessage(role="system", content="You are a Julius Caesar expert.")]
+
+mem = ChatMemoryBuffer(token_limit=1000)
+
+chat_engine = SimpleChatEngine(llm=llm, memory=mem, prefix_messages=prefix_messages)
+
+response = chat_engine.chat("whend did he die?")
+print(response)
+
+
